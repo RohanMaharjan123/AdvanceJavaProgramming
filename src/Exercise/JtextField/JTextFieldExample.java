936827ce0df6f00692964da46f3e0240abb29a01
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-05-30 19:08:53 +0545

    program to display Jtextfield on frame

---
+++ src/Exercise/JtextField/JTextFieldExample.java
@@ -0,0 +1,25 @@
+package Exercise.JTextFieldExample;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JTextFieldExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Hello");
+
+        // Create a label and a text field
+        JLabel label = new JLabel("Hi");
+        JTextField textField = new JTextField(20);
+
+        // Add components to the frame
+        frame.add(label);
+        frame.add(textField);
+
+        // Set frame properties
+        frame.setSize(200, 300);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will close the application when the frame is closed
+        frame.setVisible(true);
+    }
+}
