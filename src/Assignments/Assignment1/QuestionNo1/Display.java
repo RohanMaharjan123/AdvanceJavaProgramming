bd905edc2325b141a6dd76475c5845344d34d9fc
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-06-20 23:29:01 +0545

    completed question one of Assignment-I

---
+++ src/Assignments/Assignment1/QuestionNo1/Display.java
@@ -0,0 +1,14 @@
+package Assignment1.QuestonNo1;
+
+import javax.swing.*;
+
+public class Display {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("My First GUI Program.");
+        frame.setSize(600, 400);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
